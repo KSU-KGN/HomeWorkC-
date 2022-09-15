b39fb16ff43431a5ffe4877bfa5644a55b18c491
KSU <omom73g@gmail.com>
2022-09-16 01:46:53 +0500

    add Задача 2, Задача 4, Задача 6 и Задача 8

---
+++ Exmpl_8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Все чётные числа от 1 по {num}: ");
+int sign = 1;
+if (num < 2)
+{
+    num = -num;
+    sign = -1;
+}
+
+int step = 2;
+while (step <= num)
+{
+    Console.Write(sign * step);
+    if (num - step > 1)
+    {
+        Console.Write(", ");
+    }
+    step += 2;
+}
