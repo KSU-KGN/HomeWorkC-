c9ba951a2d3a0d5633a9f2b76fa873853661d285
KSU <omom73g@gmail.com>
2022-09-20 00:52:06 +0500

    add задача 10

---
+++ Exmpl_10/Program.cs
@@ -0,0 +1,30 @@
+﻿bool Proverka(int num)
+{
+    int count = 0;
+    //int num = number;
+
+    while (num >= 1)
+    {
+        num /= 10;
+        count++;
+    }
+
+    if (count == 3)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int num;
+do
+{
+    Console.Write("Введите трёхзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+while (Proverka(num));
+
+Console.WriteLine($"вторая цифра числа {num} равна {num / 10 % 10}");
