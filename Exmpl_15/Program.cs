9a52ba81188b35d28075c03b963c7533b497a0e5
KSU <omom73g@gmail.com>
2022-09-20 01:23:10 +0500

    add задача 15

---
+++ Exmpl_15/Program.cs
@@ -0,0 +1,28 @@
+﻿bool WeekEnd(int num)
+{
+    if (num == 6 || num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int num;
+do
+{
+    Console.Write("Введите цифру дня недели (1, 2, 3 ... 7): ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+while (num < 1 || num > 7);
+
+if (WeekEnd(num))
+{
+    Console.WriteLine("да, это выходной");
+}
+else
+{
+    Console.WriteLine("нет, это не выходной");
+}
