ba070f16cbf59c84c7ae9d104b06d3b31a9356af
KSU <omom73g@gmail.com>
2022-09-26 14:42:55 +0500

    add Задача 23

---
+++ Exmpl_23/Program.cs
@@ -0,0 +1,35 @@
+﻿int StrToInt(string text)
+{
+    int res;
+    string userLine;
+    do
+    {
+        Console.WriteLine(text);
+        userLine = Console.ReadLine();
+    }
+    while (int.TryParse(userLine, out res) == false);
+    return res;
+}
+
+int num = StrToInt("Введите число:");
+int znk;
+if (num < 0)
+{
+    znk = -1;
+}
+else
+{
+    znk = 1;    
+};
+int absNum = znk * num;
+int step = 1;
+int res;
+
+while (step < absNum)
+{
+    res = znk * step * step * step;
+    Console.Write($"{res}, ");
+    step++;
+}
+res = znk * step * step * step;
+Console.Write(res);
