bcf40c8bb5b4b2118d8c892a13df37b6550d310b
KSU <omom73g@gmail.com>
2022-09-26 14:26:01 +0500

    add Задача 21

---
+++ Exmpl_21/Program.cs
@@ -0,0 +1,30 @@
+﻿int getQuartCatet(int p1, int p2)
+{
+    int res = (p2 - p1) * (p2 - p1);
+    return res;
+}
+
+int StrToInt(string text)
+{
+    int res;
+    string userLine;
+    do
+    {
+        Console.WriteLine(text);
+        userLine = Console.ReadLine();
+    }
+    while (int.TryParse(userLine, out res) == false);
+    return res;
+}
+
+int xA, xB, yA, yB, zA, zB;
+xA = StrToInt("Введите число X точки A: ");
+yA = StrToInt("Введите число Y точки A: ");
+zA = StrToInt("Введите число Z точки A: ");
+xB = StrToInt("Введите число X точки B: ");
+yB = StrToInt("Введите число Y точки B: ");
+zB = StrToInt("Введите число Z точки B: ");
+
+double gipotenuza = Math.Sqrt(getQuartCatet(xA, xB) + getQuartCatet(yA, yB) + getQuartCatet(zA, zB));
+
+Console.WriteLine(gipotenuza);
